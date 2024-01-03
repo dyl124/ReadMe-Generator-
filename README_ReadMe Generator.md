4e685307b3a5899fe8a66178fd081baee98b1b5f
dyl124 <142150017+dyl124@users.noreply.github.com>
2024-01-03 11:08:37 +1100

    complete

---
+++ README_ReadMe Generator.md
@@ -0,0 +1,30 @@
+# ReadMe Generator
+
+## Description
+Cli interface tool to create simple, easy and aesthetic readmes for students and coders.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+Clone this repository, npm install and follow prompts in terminal by running 'node index.js'
+
+## Usage
+For students and coders who want to create fast and simple readmes.
+
+## License
+![License](https://img.shields.io/badge/License-MIT-brightgreen)
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Questions
+For questions about this project, please contact [dyl124](https://github.com/dyl124) at Dylan Main.
+
+## Tests
+
+
+## Screenshots
+screenshot.png
---
+++ index.js
@@ -0,0 +1,140 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+let title;
+let displayedFiles = [];
+
+async function promptUser() {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Enter the project title:',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter the project description:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Enter installation instructions:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Enter usage information:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license:',
+      choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'contributors',
+      message: 'Enter contributors:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Please copy and paste any relevant test information:',
+    },
+    {
+      type: 'checkbox',
+      name: 'screenshots',
+      message: 'Please select your screenshots',
+      choices: displayedFiles, 
+    },
+  ]);
+}
+
+function generateREADME(results) {
+  title = results.title;
+  return `# ${results.title}
+
+## Description
+${results.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+${results.installation}
+
+## Usage
+${results.usage}
+
+## License
+![License](https://img.shields.io/badge/License-${results.license}-brightgreen)
+
+This project is licensed under the ${results.license} License - see the [LICENSE](LICENSE) file for details.
+
+## Questions
+For questions about this project, please contact [${results.github}](https://github.com/${results.github}) at ${results.contributors}.
+
+## Tests
+${results.tests}
+
+## Screenshots
+${results.screenshots}`;
+}
+
+function displayDirectory(directoryPath) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(directoryPath, (err, files) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      displayedFiles = files;
+      resolve(displayedFiles);
+    });
+  });
+}
+
+// Function to write content to file
+function writeToFile(fileName, content) {
+  fs.writeFile(fileName, content, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`${fileName} successfully generated!`);
+    }
+  });
+}
+
+async function init() {
+  try {
+    // Display the directory contents and log displayedFiles
+    await displayDirectory('./screenshots');
+  
+
+    // Prompt the user for input
+    const userResults = await promptUser();
+
+    // Generate README content
+    const readmeContent = generateREADME(userResults);
+
+    // Write to file
+    const fileName = `README_${title}.md`;
+    writeToFile(fileName, readmeContent);
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Run the application
+init();
